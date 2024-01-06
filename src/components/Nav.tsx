2f38e1f14f565a5a4f6da37ac4202659497f26f8
huseyingulec <itshuseyingulec@gmail.com>
2024-01-06 20:01:23 +0100

    chore: change some design

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -33,46 +33,47 @@ const Nav = () => {
     };
     return (
         <div className="shadow-md w-full sticky top-0 bg-white dark:bg-black dark:text-white dark:shadow-slate-900">
-            <div className="md:flex items-center justify-between pt-2 md:py-2.5 md:px-12 px-7 max-w-screen-xl mx-auto">
-                <div className="font-bold text-2xl cursor-pointer flex items-center font-mono pl-5">
+            <div className="md:flex items-center justify-between md:pl-12 px-7 py-3 max-w-screen-xl mx-auto">
+                <div className="font-bold text-2xl cursor-pointer font-mono">
                     S55
                 </div>
 
                 <div
                     onClick={() => setOpen(!open)}
-                    className="text-3xl absolute right-8 top-1 cursor-pointer md:hidden "
+                    className="text-3xl absolute right-8 top-3 cursor-pointer md:hidden "
                 >
                     <ion-icon name={open ? "close" : "menu"}></ion-icon>
                 </div>
 
                 <ul
-                    className={`md:flex md:items-center md:pb-0 shadow-md md:shadow-none absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-12 pb-5 transition-all ease-in dark:shadow-slate-900 cursor-pointer md:px-10 ${
-                        open ? "top-6 bg-white dark:bg-black" : "top-[-490px]"
+                    className={`md:flex md:items-center md:pb-0 shadow-md md:shadow-none absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto pl-10 transition-all ease-in dark:shadow-slate-900 cursor-pointer ${
+                        open ? "top-14 bg-white dark:bg-black" : "top-[-490px]"
                     }`}
                 >
                     {Links.map(link => (
                         <li
                             key={link.name}
-                            className="md:ml-10 text-xl md:my-0 my-7 "
+                            className="md:mx-5 text-xl md:my-0 my-4 "
                         >
                             <a
                                 href={link.link}
-                                className="no-underline  hove:text-gree"
+                                className="no-underline"
                             >
                                 {link.name}
                             </a>
                         </li>
                     ))}
 
-                    <span
+                    <div
                         onClick={() => {
                             setIsDarkMode(!isDarkMode);
                             toggleTheme(!isDarkMode);
+                            setOpen(false); // Close the menu
                         }}
-                        className="text-xl my-7 md:ml-10 md:my-3 md:pt-1 "
+                        className="text-xl md:mx-5 my-4 md:my-0 h-6"
                     >
                         <ion-icon name="moon"></ion-icon>
-                    </span>
+                    </div>
                 </ul>
             </div>
         </div>
