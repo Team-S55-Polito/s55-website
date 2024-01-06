be602e322968159c0ec8ba3d91fb731639eca993
huseyingulec <itshuseyingulec@gmail.com>
2024-01-06 03:09:30 +0100

    feat: add new navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,46 +1,82 @@
-import "./nav.scss";
-import logo from "../assets/teams55-logo.png";
+import { useState, useEffect } from "react";
 
-export default function Nav() {
-    return (
-        <nav>
-            {/* Navbar header */}
-            <a href="#home" className="nav--header">
-                <img className="nav--logo" src={logo} alt="World Logo" />
-                <h4 className="nav--title">Team S55</h4>
-            </a>
+const Nav = () => {
+    let Links = [
+        { name: "Home", link: "/" },
+        { name: "About", link: "/" },
+        { name: "Team", link: "/" },
+        { name: "Sponsors", link: "/" },
+        { name: "Contact", link: "/" },
 
-            {/* Burger icon */}
-            <div>
-                <input type="checkbox" id="burger-toggle" />
-                <label htmlFor="burger-toggle" id="burger" className="burger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </label>
+    ];
+    let [open, setOpen] = useState(false);
 
-                {/* Navbar menu */}
-                <div id="navbar--menu">
-                    <ul>
-                        <li>
-                            <a href="#home">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#">Link 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Link 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Link 3</a>
-                        </li>
-                        
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    const [isDarkMode, setIsDarkMode] = useState(
+        window.matchMedia("(prefers-color-scheme: dark)").matches
     );
-}
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        const handleChange = () => setIsDarkMode(mediaQuery.matches);
+        mediaQuery.addEventListener("change", handleChange);
+        // Set the initial theme based on the system preference
+        toggleTheme(isDarkMode);
+        return () => mediaQuery.removeEventListener("change", handleChange);
+    }, []);
+
+    const toggleTheme = darkMode => {
+        if (darkMode) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    };
+    return (
+        <div className="shadow-md w-full sticky top-0 bg-white dark:bg-black dark:text-white dark:shadow-slate-900">
+            <div className="md:flex items-center justify-between pt-2 md:py-2.5 md:px-12 px-7 max-w-screen-xl mx-auto">
+                <div className="font-bold text-2xl cursor-pointer flex items-center font-mono pl-5">
+                    S55
+                </div>
+
+                <div
+                    onClick={() => setOpen(!open)}
+                    className="text-3xl absolute right-8 top-1 cursor-pointer md:hidden "
+                >
+                    <ion-icon name={open ? "close" : "menu"}></ion-icon>
+                </div>
+
+                <ul
+                    className={`md:flex md:items-center md:pb-0 shadow-md md:shadow-none absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-12 pb-5 transition-all ease-in dark:shadow-slate-900 cursor-pointer md:px-10 ${
+                        open ? "top-6 bg-white dark:bg-black" : "top-[-490px]"
+                    }`}
+                >
+                    {Links.map(link => (
+                        <li
+                            key={link.name}
+                            className="md:ml-10 text-xl md:my-0 my-7 "
+                        >
+                            <a
+                                href={link.link}
+                                className="no-underline  hove:text-gree"
+                            >
+                                {link.name}
+                            </a>
+                        </li>
+                    ))}
+
+                    <span
+                        onClick={() => {
+                            setIsDarkMode(!isDarkMode);
+                            toggleTheme(!isDarkMode);
+                        }}
+                        className="text-xl my-7 md:ml-10 md:my-3 md:pt-1 "
+                    >
+                        <ion-icon name="moon"></ion-icon>
+                    </span>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
